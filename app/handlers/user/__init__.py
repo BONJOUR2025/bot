8867558c3b3f37f8391d55faf0d57b2757145d36
BONJOUR2025 <hr.bonjourspb@gmail.com>
2025-07-23 23:33:37 +0300

    Export user handlers

--- app/handlers/user/__init__.py
+++ app/handlers/user/__init__.py
@@ -1,3 +1,43 @@
-from . import payout
+"""Expose public user handlers for convenient imports."""
 
-__all__ = ["payout"]
+from . import payout
+from .start import start
+from .home import get_user_info_user, home_handler_user
+from .menu import (
+    view_salary_user,
+    view_schedule_user,
+    handle_selected_month_user,
+)
+from .salary import handle_salary_request
+from .cabinet import (
+    personal_cabinet,
+    view_user_info,
+    edit_user_info,
+    handle_edit_selection,
+    save_new_value,
+    view_request_history,
+    handle_edit_confirmation,
+    handle_admin_change_response,
+)
+from .ack import handle_acknowledgment
+
+
+__all__ = [
+    "start",
+    "get_user_info_user",
+    "home_handler_user",
+    "view_salary_user",
+    "view_schedule_user",
+    "handle_selected_month_user",
+    "handle_salary_request",
+    "personal_cabinet",
+    "view_user_info",
+    "edit_user_info",
+    "handle_edit_selection",
+    "save_new_value",
+    "view_request_history",
+    "handle_edit_confirmation",
+    "handle_admin_change_response",
+    "handle_acknowledgment",
+    "payout",
+]
