2fff78a728ec7e3e2b3b37b2fff1a6ce6042e0f5
BONJOUR2025 <hr.bonjourspb@gmail.com>
2025-07-23 23:04:04 +0300

    feat: enhance admin UI components

--- admin_frontend/tailwind.config.js
+++ admin_frontend/tailwind.config.js
@@ -2,12 +2,20 @@ import colors from 'tailwindcss/colors';
 import { fontFamily } from 'tailwindcss/defaultTheme';
 
 export default {
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
         primary: '#FF6600',
         secondary: '#666666',
+        brand: {
+          DEFAULT: '#FF6600',
+          light: '#FFD6B3',
+          dark: '#CC5200',
+        },
+        surface: '#F9F9FB',
+        accent: '#3B82F6',
       },
       fontFamily: {
         sans: ['Roboto', ...fontFamily.sans],
