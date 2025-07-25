73f165beb7d17a159266d11869db7d8f70b6ef29
BONJOUR2025 <hr.bonjourspb@gmail.com>
2025-07-25 18:05:27 +0300

    Show admin panel directly

--- admin_frontend/src/App.jsx
+++ admin_frontend/src/App.jsx
@@ -1,13 +1,5 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import FullDemo from './full/App.tsx';
+import { AdminPanel } from './full/components/admin/AdminPanel';
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/*" element={<FullDemo />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
-  );
+  return <AdminPanel />;
 }
