43ab3469cdeff0a6d40c9727e1e5be27027873d6
BONJOUR2025 <hr.bonjourspb@gmail.com>
2025-07-25 17:11:16 +0300

    Connect admin UI to API

--- admin_frontend/src/api.js
+++ admin_frontend/src/api.js
@@ -20,3 +20,28 @@ api.interceptors.response.use(
 );
 
 export default api;
+
+export async function getEmployees() {
+  const res = await api.get('/employees/');
+  return res.data;
+}
+
+export async function getActivePayouts() {
+  const res = await api.get('/payouts/active');
+  return res.data;
+}
+
+export async function getActiveVacations() {
+  const res = await api.get('/vacations/active');
+  return res.data;
+}
+
+export async function getBirthdays(days = 7) {
+  const res = await api.get(`/birthdays/?days=${days}`);
+  return res.data;
+}
+
+export async function getSales() {
+  const res = await api.get('/analytics/sales');
+  return res.data;
+}
